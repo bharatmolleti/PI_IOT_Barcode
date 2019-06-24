7393bde090315d26b2e1ed46aa6d590c45b875c3
Bharat K Molleti <bharatmolleti@gmail.com>
2019-06-24 07:25:57 +0100

    fixing to page to serve up all request

--- jsonBarcode.php
+++ jsonBarcode.php
@@ -10,12 +10,13 @@ try {
             // Try Connect to the DB with new MySqli object - Params {hostname, userid, password, dbname}
             $mysqli = new mysqli($servername, $username, $password, $database);
 
-            $statement = $mysqli->prepare("select * from barcodeLog;");
+	    $statement = $mysqli->prepare("select * from barcodeLog;");
 
             $statement->execute(); // Execute the statement.
             $result = $statement->get_result(); // Binds the last executed statement as a result.
 
-            echo json_encode(($result->fetch_array())); // Parse to JSON and print.
+	    echo json_encode($result->fetch_all(MYSQLI_ASSOC));
+            //echo json_encode(($result->fetch_array())); // Parse to JSON and print.
 
         } catch (mysqli_sql_exception $e) { // Failed to connect? Lets see the exception details..
             echo "MySQLi Error Code: " . $e->getCode() . "<br />";
